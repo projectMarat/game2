8ef7473d7b95ec3ab2cb55a769493a53967457a4
projectMarat <57598293+projectMarat@users.noreply.github.com>
2020-05-20 17:27:51 +0300

    4 level fix again

--- core/src/com/mygdx/game2/Screens/PlayScreen.java
+++ core/src/com/mygdx/game2/Screens/PlayScreen.java
@@ -113,6 +113,7 @@ public class PlayScreen implements Screen {
 
         //Load our map and setup our map renderer
         maploader = new TmxMapLoader();
+        if(values.worldNumber>values.maxWorld)values.worldNumber=1;
         map = maploader.load("maps/map"+values.worldNumber+".tmx");
 
 
